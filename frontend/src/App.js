820e3f42b8d1c7b0a81d9c4b2ddaad46f2415497
Alex M <1maltsevalex@gmail.com>
2020-12-23 11:39:58 +0200

    003-Starting The Front End
    => Home Screen
    => Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Home_Screen from './screens/Home_Screen';
 
 const App = () => {
  return (
@@ -8,7 +9,7 @@ const App = () => {
    <Header />
    <main className='py-3'>
     <Container>
-     <h1>Welcome to ProShop</h1>
+     <Home_Screen />
     </Container>
    </main>
    <Footer />
