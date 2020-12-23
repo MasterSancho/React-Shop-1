6fd2e9d7ab11e166cfa7912e989860016a0d61de
Alex M <1maltsevalex@gmail.com>
2020-12-23 12:02:56 +0200

    005-Starting The Front End
    => Implementing React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/Home_Screen';
+import ProductScreen from './screens/Product_Screen';
 
 const App = () => {
  return (
-  <>
+  <Router>
    <Header />
    <main className='py-3'>
     <Container>
-     <HomeScreen />
+     <Route path='/' component={HomeScreen} exact />
+     <Route path='/product/:id' component={ProductScreen} exact />
     </Container>
    </main>
    <Footer />
-  </>
+  </Router>
  );
 };
 
