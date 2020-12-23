e1b2047929bc6bd3c96a61304ffe2cc976489196
Alex M <1maltsevalex@gmail.com>
2020-12-23 09:48:51 +0200

    001-Starting The Front End
    => React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+const App = () => {
+ return (
+  <>
+   <Header />
+   <main>
+    <h1>Welcome to ProShop</h1>
+   </main>
+   <Footer />
+  </>
+ );
+};
+
+export default App;
