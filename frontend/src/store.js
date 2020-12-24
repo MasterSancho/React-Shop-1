a63a962a5e53e1e22ba047758d001461ccd69254
Alex M <1maltsevalex@gmail.com>
2020-12-24 14:38:27 +0200

    020-Implementing Redux For State Management
    => Product Details Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from './reducers/product_Reducers';
+import {
+ productListReducer,
+ productDetailsReducer,
+} from './reducers/product_Reducers';
 
 const reducer = combineReducers({
  productList: productListReducer,
+ productDetails: productDetailsReducer,
 });
 
 const initialState = {};
