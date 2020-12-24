5bce3a7b4ca61eb1e9a7063dbedbe3b9f66c185e
Alex M <1maltsevalex@gmail.com>
2020-12-24 17:36:12 +0200

    025-Adding The Shopping Cart
    35 => Remove Items From Cart

--- frontend/src/screens/Cart_Screen.js
+++ frontend/src/screens/Cart_Screen.js
@@ -11,7 +11,7 @@ import {
  Card,
 } from 'react-bootstrap';
 import Message from '../components/Message';
-import { addToCart } from '../actions/cart_Actions';
+import { addToCart, removeFromCart } from '../actions/cart_Actions';
 
 const CartScreen = ({ match, location, history }) => {
  const productId = match.params.id;
@@ -30,7 +30,7 @@ const CartScreen = ({ match, location, history }) => {
  }, [dispatch, productId, qty]);
 
  const removeFromCartHandler = (id) => {
-  console.log('Removed');
+  dispatch(removeFromCart(id));
  };
 
  const checkoutHandler = () => {
