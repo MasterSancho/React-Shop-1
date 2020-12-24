7d64002bae93bb21e719f8fd1c083b794c83e60c
Alex M <1maltsevalex@gmail.com>
2020-12-24 19:39:57 +0200

    029-Back End User Authentication
    40 => Custom Authentication Middleware

--- backend/routes/user_Routes.js
+++ backend/routes/user_Routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/user_Controller.js';
+import { authUser, getUserProfile } from '../controllers/user_Controller.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
