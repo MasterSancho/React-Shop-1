d6f29134bdc1755fde9b4044c91e587fe27534a2
Alex M <1maltsevalex@gmail.com>
2020-12-24 11:19:02 +0200

    015-Getting Started With MongoDB
    => Custom Error Handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -10,6 +10,7 @@ router.get(
  '/',
  asyncHandler(async (req, res) => {
   const products = await Product.find({});
+
   res.json(products);
  })
 );
@@ -21,10 +22,12 @@ router.get(
  '/:id',
  asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
+
   if (product) {
    res.json(product);
   } else {
-   res.status(404).json({ message: 'Product not found' });
+   res.status(404);
+   throw new Error('Product not found');
   }
  })
 );
