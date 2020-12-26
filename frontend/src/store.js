c071823c2370d7a23d6789e0f506beb6aa6e7751
Alex M <1maltsevalex@gmail.com>
2020-12-26 17:10:09 +0200

    044-Checkout Process - Part 1
    56 => Create Order

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,6 +12,7 @@ import {
  userDetailsReducer,
  userUpdateProfileReducer,
 } from './reducers/user_Reducers';
+import { orderCreateReducer } from './reducers/order_Reducers';
 
 const reducer = combineReducers({
  productList: productListReducer,
@@ -21,6 +22,7 @@ const reducer = combineReducers({
  userRegister: userRegisterReducer,
  userDetails: userDetailsReducer,
  userUpdateProfile: userUpdateProfileReducer,
+ orderCreate: orderCreateReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
