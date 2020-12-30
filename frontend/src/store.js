8d897e6ca9d9cde6e9a61f08e64af4c2593640dc
Alex M <1maltsevalex@gmail.com>
2020-12-30 11:49:34 +0200

    061-Admin Screens - Part 2
    74 => Admin Delete Products

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import {
  productListReducer,
  productDetailsReducer,
+ productDeleteReducer,
 } from './reducers/product_Reducers';
 import { cartReducer } from './reducers/cart_Reducers';
 import {
@@ -25,6 +26,7 @@ import {
 const reducer = combineReducers({
  productList: productListReducer,
  productDetails: productDetailsReducer,
+ productDelete: productDeleteReducer,
  cart: cartReducer,
  userLogin: userLoginReducer,
  userRegister: userRegisterReducer,
